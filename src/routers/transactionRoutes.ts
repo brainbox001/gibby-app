c4c949eeb3e0343dd1671cf8d49e994d3735d1b8
Daniel Marcus <130494114+brainbox001@users.noreply.github.com>
2024-12-09 21:19:29 +0100

    Update transactionRoutes.ts

--- src/routers/transactionRoutes.ts
+++ src/routers/transactionRoutes.ts
@@ -3,8 +3,14 @@ import newDeposit from '../controllers/transactions/newDeposit';
 import verifyPayment from '../controllers/transactions/payStackVerify';
 import getSavings from '../controllers/transactions/getSavings';
 import getTransactions from '../controllers/transactions/getTransactions';
+import cors from 'cors';
 
 const transactionRoutes = express.Router();
+
+app.use(cors({
+  origin: 'https://gibby-frontend.onrender.com',
+  credentials: true,
+}));
 
 transactionRoutes.get('/', getTransactions);
 transactionRoutes.get('/savings', getSavings);
